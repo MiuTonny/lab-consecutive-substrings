63217774a7d98ab552f8d19e3a1bfdb111ea2c98
root <root@tony.>
2025-06-18 18:43:30 -0400

    Update lab summary and add beginner homework

--- consecutive_substrings.js
+++ consecutive_substrings.js
@@ -1,22 +1,22 @@
-function consecutiveSubstrings(string) {
+function consecutiveSubstrings(string) { //function for consecutive substrings
   console.log("Input received:", string); //debugging
 
-  const substrings = [];
+  const substrings = [];//empty array for storage
 
-  for (let start = 0; start < string.length; start++) {
-    for (let end = start + 1; end <= string.length; end++) {
-      substrings.push(string.slice(start, end));
+  for (let start = 0; start < string.length; start++) { //outer loop go eacg letter
+    for (let end = start + 1; end <= string.length; end++) { //inner loop: build substrings adding 1 letter until the end
+      substrings.push(string.slice(start, end)); //extract substring 
     }
   }
 
-  return substrings;
+  return substrings; //return the list of substrings
 }
 
 module.exports = consecutiveSubstrings;
 
 
 if (require.main === module) {
-  // add your own tests in here
+  
   console.log("Expecting: ['a', 'ab', 'abc', 'b', 'bc', 'c']");
   console.log("=>", consecutiveSubstrings('abc'));
 
